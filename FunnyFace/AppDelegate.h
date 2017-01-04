a5558250fab9b3d368143894ee2b83c2b9f01c5c
Anton <anton.nadtoka@yandex.ru>
2017-01-04 19:14:26 +0300

    First commit

---
+++ FunnyFace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunnyFace
+//
+//  Created by antondev on 04/01/17.
+//  Copyright © 2017 engune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
